6298d59c15e01d5dc4d18f66096cd556ff4e54f1
Stefano Labianca <lstefano@proton.me>
2026-02-25 22:46:32 +0100

    first iteration on creating a bag of words

--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type BoW = map[string]int64
+
+func readContent(path string) string {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(data)
+}
+
+func cleanFileContent(content string, what string) string {
+	return strings.ReplaceAll(content, what, "")
+}
 
 func main() {
-	fmt.Println("Hello")
+	bow := BoW{}
+
+	err := filepath.WalkDir("enron1", func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			panic(err)
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		content := readContent(path)
+		content = cleanFileContent(content, "-")
+		content = cleanFileContent(content, "/")
+		content = cleanFileContent(content, ";")
+		content = cleanFileContent(content, ".")
+		content = cleanFileContent(content, ",")
+		content = cleanFileContent(content, "@")
+		content = cleanFileContent(content, "(")
+		content = cleanFileContent(content, ")")
+		content = cleanFileContent(content, ":")
+		content = cleanFileContent(content, "~")
+		content = cleanFileContent(content, "{")
+		content = cleanFileContent(content, "}")
+
+		tokens := strings.FieldsSeq(content)
+
+		for token := range tokens {
+			bow[token] += 1
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	for token := range bow {
+		fmt.Printf("%v => %v\n", token, bow[token])
+	}
 }
