db85467c9a04c91de9c8bf0f046d5f5cd2b4e7de
Stefano Labianca <lstefano@proton.me>
2026-02-27 22:18:44 +0100

    first implementation for the KNN classifier

--- classification/knn/knn.go
+++ classification/knn/knn.go
@@ -1,1 +1,86 @@
 package knn
+
+import (
+	"math"
+	"ml/common"
+	"os"
+	"path"
+	"sort"
+)
+
+type Point struct {
+	Document *common.BoW
+	Class    string
+}
+
+type KNNModel struct {
+	Points []Point
+	V      *common.Vocabulary
+}
+
+type NearestPoints struct {
+	Distance float64
+	Index    int
+	Class    string
+}
+
+const initialCapacity int = 10_000
+
+func Train(folders []string) KNNModel {
+	db := make([]Point, 0, initialCapacity)
+	v := common.Vocabulary{}
+	classes := []string{"ham", "spam"}
+
+	for _, folder := range folders {
+		for _, class := range classes {
+			documents, err := os.ReadDir(path.Join(folder, class))
+			if err != nil {
+				panic(err)
+			}
+
+			for _, document := range documents {
+				bow := common.BoW{}
+				common.ReadClassDocument(folder, class, document.Name(), &bow)
+				db = append(db, Point{Document: &bow, Class: class})
+				common.CreateVocabulary(&bow, &v)
+			}
+		}
+	}
+
+	return KNNModel{
+		Points: db,
+		V:      &v,
+	}
+}
+
+func Fit(model KNNModel, p *common.BoW, k int) []NearestPoints {
+	distances := []NearestPoints{}
+
+	if k <= 0 {
+		panic("k parameter must be greater than 0")
+	}
+
+	for index, q := range model.Points {
+		d := euclideanDistance(p, q.Document, model.V)
+		distances = append(distances, NearestPoints{Distance: d, Index: index, Class: q.Class})
+	}
+
+	sort.Slice(distances, func(i int, j int) bool {
+		return distances[i].Distance < distances[j].Distance
+	})
+
+	return distances[:k]
+}
+
+func euclideanDistance(p *common.BoW, q *common.BoW, v *common.Vocabulary) float64 {
+	var distance float64
+
+	for token := range *v {
+		x := (*p)[token]
+		y := (*q)[token]
+		squareValue := math.Pow(float64(x-y), 2)
+		distance += squareValue
+	}
+
+	return math.Sqrt(distance)
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import example "ml/examples"
+import (
+	"fmt"
+	"ml/classification/knn"
+	"ml/common"
+)
 
 func main() {
-	example.RunBayes()
+	folders := []string{"enron1", "enron4"}
+	model := knn.Train(folders)
+	bow := common.BoW{}
+	common.ReadClassDocument("enron2", "ham", "0020.1999-12-14.kaminski.ham.txt", &bow)
+	res := knn.Fit(model, &bow, 3)
+
+	for _, n := range res {
+		fmt.Printf("Class => %v\n", n.Class)
+		fmt.Printf("Index => %v\n", n.Index)
+		fmt.Printf("Distance => %v\n", n.Distance)
+		fmt.Printf("-------------------\n")
+	}
 }
