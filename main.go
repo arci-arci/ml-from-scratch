7d2bd87051750cdc50c4e5f30b364ff7b2926f7f
Stefano Labianca <lstefano@proton.me>
2026-02-28 00:42:13 +0100

    a more cleaner example

--- main.go
+++ main.go
@@ -4,15 +4,35 @@ import (
 	"fmt"
 	"ml/classification/knn"
 	"ml/common"
+	"os"
+	"path"
 )
 
 func main() {
-	folders := []string{"enron1", "enron4"}
+	folders := []string{"enron1"}
 	model := knn.Train(folders)
-	bow := common.BoW{}
-	k := 100
-	common.ReadClassDocument("enron2", "ham", "0020.1999-12-14.kaminski.ham.txt", &bow)
-	// common.ReadClassDocument("enron2", "spam", "0026.2001-07-13.SA_and_HP.spam.txt", &bow)
-	class := knn.Fit(model, &bow, k)
-	fmt.Printf("Class => %v\n", class)
+	k := 10
+	documents, err := os.ReadDir(path.Join("enron2", "ham"))
+	if err != nil {
+		panic(err)
+	}
+
+	var tp int
+	var fn int
+
+	for _, doc := range documents {
+		bow := common.BoW{}
+		common.ReadClassDocument("enron2", "ham", doc.Name(), &bow)
+		class := knn.Fit(model, &bow, k)
+		fmt.Printf("Document %v Class => %v\n", doc.Name(), class)
+
+		if class == "ham" {
+			tp += 1
+		} else {
+			fn += 1
+		}
+	}
+
+	fmt.Printf("Recall = %v\n", float64(tp)/float64(tp+fn))
+
 }
