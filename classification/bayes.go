785e94524ad27b154adf7c2fcc6909620e7200bb
Stefano Labianca <lstefano@proton.me>
2026-02-27 15:54:44 +0100

    improved modularity

---
+++ classification/bayes.go
@@ -0,0 +1,74 @@
+package bayes
+
+import (
+	"math"
+	"ml/common"
+)
+
+type probabilities struct {
+	ham  float64
+	spam float64
+}
+
+type ClassificationOptions struct {
+	HamProb    float64
+	SpamProb   float64
+	Classifier *bayesClassifier
+	Doc        *common.BoW
+}
+
+type BayesOptions struct {
+	HamBoW     *common.BoW
+	SpamBoW    *common.BoW
+	Vocabulary *common.Vocabulary
+}
+
+type ClassificationResult struct {
+	Ham  float64
+	Spam float64
+}
+
+type bayesClassifier = map[string]probabilities
+
+func Train(options BayesOptions) bayesClassifier {
+	classifier := bayesClassifier{}
+	termsForHam := common.CalcualteTermsAmount(options.HamBoW)
+	termsForSpam := common.CalcualteTermsAmount(options.SpamBoW)
+	vSize := len(*options.Vocabulary)
+
+	for token := range *options.Vocabulary {
+		// Probability calculated based on Laplace Correction
+		probForHam := float64((*options.HamBoW)[token]+1) / float64(termsForHam+int64(vSize))
+		probForSpam := float64((*options.SpamBoW)[token]+1) / float64(termsForSpam+int64(vSize))
+		tProb := probabilities{
+			ham:  probForHam,
+			spam: probForSpam,
+		}
+
+		classifier[token] = tProb
+	}
+
+	return classifier
+}
+
+func Classify(options ClassificationOptions) ClassificationResult {
+	docInHam := 0.0
+	docInSpam := 0.0
+
+	for token := range *options.Doc {
+		tProb := (*options.Classifier)[token]
+
+		// Skipping rare tokens
+		if tProb.ham == 0 || tProb.spam == 0 {
+			continue
+		}
+
+		docInHam += math.Log(tProb.ham)
+		docInSpam += math.Log(tProb.spam)
+	}
+
+	return ClassificationResult{
+		Ham:  docInHam + math.Log(options.HamProb),
+		Spam: docInSpam + math.Log(options.SpamProb),
+	}
+}
---
+++ common/common.go
@@ -0,0 +1,116 @@
+package common
+
+import (
+	"io/fs"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+type BoW = map[string]int64
+type Vocabulary = map[string]int64
+
+func ReadContent(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+func GetDocAmount(root string, class string) int {
+	docs, dirErr := os.ReadDir(path.Join(root, class))
+	if dirErr != nil {
+		panic(dirErr)
+	}
+
+	return len(docs)
+}
+
+func CalcualteTermsAmount(bow *BoW) int64 {
+	var total int64
+	for t := range *bow {
+		total += (*bow)[t]
+	}
+
+	return total
+}
+
+func CleanFileContent(content string) string {
+	removableSymbols := []string{
+		"-",
+		"/",
+		";",
+		".",
+		",",
+		"@",
+		"(",
+		")",
+		":",
+		"~",
+		"{",
+		"}",
+		">",
+		"<",
+	}
+
+	for _, symbol := range removableSymbols {
+		content = strings.ReplaceAll(content, symbol, "")
+	}
+
+	return content
+}
+
+func CreateVocabulary(bow *BoW, v *Vocabulary) {
+	for t := range *bow {
+		(*v)[t] += (*bow)[t]
+	}
+}
+
+func ReadClassDocuments(root string, class string, bow *BoW) error {
+	path := path.Join(root, class)
+	err := filepath.WalkDir(path, func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			panic(err)
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		content, err := ReadContent(path)
+		if err != nil {
+			panic(err)
+		}
+
+		content = CleanFileContent(content)
+		tokens := strings.FieldsSeq(content)
+
+		for token := range tokens {
+			(*bow)[token] += 1
+		}
+
+		return nil
+	})
+
+	return err
+}
+
+func ReadClassDocument(root string, class string, fileName string, bow *BoW) error {
+	path := path.Join(root, class, fileName)
+	content, err := ReadContent(path)
+	if err != nil {
+		panic(err)
+	}
+
+	content = CleanFileContent(content)
+	tokens := strings.FieldsSeq(content)
+
+	for token := range tokens {
+		(*bow)[token] += 1
+	}
+
+	return nil
+}
