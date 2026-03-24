4bf48f2b91ba159435b1943898178a78a6895711
Stefano Labianca <lstefano@proton.me>
2026-03-24 17:12:50 +0100

    major improvements

--- example/linear.go
+++ example/linear.go
@@ -2,15 +2,15 @@ package example
 
 import (
 	"fmt"
+	"math"
 	linear "ml/regression"
 )
 
 func RunLinearRegression() {
 	dataset := linear.ReadCSV()
 	distribution := linear.Distribution{
-		Train:      0.7,
-		Validation: 0.2,
-		Test:       0.1,
+		Train: 0.7,
+		Test:  0.3,
 	}
 	seed := linear.Seed{
 		FirstSeed: 42,
@@ -18,33 +18,63 @@ func RunLinearRegression() {
 	}
 
 	target := 9 // color_intensity
+	train, test := linear.SplitDataset(dataset, distribution, seed)
+	yTrain := linear.GetTarget(train, target)
 
-	// for i, e := range dataset {
-	// 	if i%10 != 0 || i == 0 {
-	// 		fmt.Printf("%v  ", e[target])
-	// 	} else {
-	// 		fmt.Printf("%v\n", e[target])
-	// 	}
-	// }
+	scaler := linear.FitStandardScaler(train, target)
+	scaledTrain := linear.Transform(scaler, train, target)
+	scaledTest := linear.Transform(scaler, test, target)
 
-	// fmt.Printf("\n\n-------------------\n\n")
-
-	train, val, test := linear.SplitDataset(dataset, distribution, seed)
 	options := linear.LinearRegressionOpt{
-		Train:        train,
-		Validation:   val,
-		LearningRate: 0.05,
-		InputFeature: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11},
+		Train:        scaledTrain,
+		LearningRate: 0.0002,
+		InputFeature: []int{0, 1, 4},
 		Target:       target,
-		BatchSize:    5,
-		BatchSeed: linear.Seed{
-			FirstSeed: 10,
-			SeconSeed: 1000,
-		},
+		BatchSize:    10,
+		Alpha:        0.01,
+		Epochs:       600,
 	}
 
 	model := linear.Train(options)
-	example := test[6]
-	pred := linear.Fit(model, example, target)
-	fmt.Printf("Prediction %v => Real Value %v\n", pred, example[target])
+
+	fmt.Print("\n\n------- Test step -------\n\n")
+
+	predictions := make([]float64, len(scaledTest))
+	trueValues := make([]float64, len(scaledTest))
+
+	for i, example := range scaledTest {
+		scaledPred := linear.Fit(model, example)
+		pred := linear.InverseTransform(scaler, scaledPred, target)
+		trueV := yTrain[i]
+
+		predictions[i] = pred
+		trueValues[i] = trueV
+
+		fmt.Printf("Pred: %v, Real: %v\n", pred, trueV)
+	}
+
+	fmt.Print("\n\n------- MAE -------\n\n")
+	fmt.Printf("MAE: %v", linear.Mae(predictions, trueValues))
+
+}
+
+func Correlation(x, y []float64) float64 {
+	n := float64(len(x))
+	meanX, meanY := 0.0, 0.0
+	for i := range x {
+		meanX += x[i]
+		meanY += y[i]
+	}
+	meanX /= n
+	meanY /= n
+
+	num, denX, denY := 0.0, 0.0, 0.0
+	for i := range x {
+		dx := x[i] - meanX
+		dy := y[i] - meanY
+		num += dx * dy
+		denX += dx * dx
+		denY += dy * dy
+	}
+	return num / math.Sqrt(denX*denY)
 }
--- regression/linear.go
+++ regression/linear.go
@@ -13,9 +13,8 @@ import (
 
 type CSVData = [][]float64
 type Distribution struct {
-	Train      float64
-	Validation float64
-	Test       float64
+	Train float64
+	Test  float64
 }
 
 type Seed struct {
@@ -25,12 +24,12 @@ type Seed struct {
 
 type LinearRegressionOpt struct {
 	Train        CSVData
-	Validation   CSVData
 	InputFeature []int
 	LearningRate float64
 	Target       int
 	BatchSize    int
-	BatchSeed    Seed
+	Alpha        float64
+	Epochs       int
 }
 
 type LinearRegressionModel struct {
@@ -38,36 +37,51 @@ type LinearRegressionModel struct {
 	Xs      []int
 }
 
-func Train(options LinearRegressionOpt) LinearRegressionModel {
-	epochs := int(math.Floor(float64(len(options.Train)) / float64(options.BatchSize)))
-	fmt.Printf("Epochs amount: %v\n", epochs)
+type StandardScaler struct {
+	Mean []float64
+	Std  []float64
+}
 
-	// TODO:
-	// 	- Scale values
-	// 	- Implement regularization
+func Train(options LinearRegressionOpt) LinearRegressionModel {
+	batches := int(math.Floor(float64(len(options.Train)) / float64(options.BatchSize)))
+	fmt.Printf("Epochs amount: %v\n", batches)
 
 	// Adding 1 so I can take into account the intercept
 	Xs := addIntercept(options.InputFeature)
 	weights := initWeights(len(Xs))
 	fmt.Printf("Genereted weights: %v\n", weights)
 
+	currentBatch := 1
 	currentEpoch := 1
-	for currentEpoch <= epochs {
-		batchWeights := initBatchWeights(len(Xs))
-		batch := createBatch(options.Train, options.BatchSeed, options.BatchSize)
 
-		for _, e := range batch {
-			// Adding X0 value
-			example := append([]float64{1.0}, e...)
-			err := prediction(example, weights, Xs) - example[options.Target+1]
+	for currentEpoch <= options.Epochs {
+		fmt.Printf("Current epoch: %v\n", currentEpoch)
 
-			for i, idx := range Xs {
-				batchWeights[i] = batchWeights[i] + err*example[idx]
+		rand.Shuffle(options.BatchSize, func(i, j int) {
+			options.Train[i], options.Train[j] = options.Train[j], options.Train[i]
+		})
+
+		for currentBatch <= batches {
+			batchWeights := initBatchWeights(len(Xs))
+			batch := createBatch(options.Train, options.BatchSize)
+
+			for _, e := range batch {
+				// Adding X0 value
+				example := append([]float64{1.0}, e...)
+				err := 0.5 * math.Pow(prediction(example, weights, Xs)-example[options.Target+1], 2)
+
+				for i, idx := range Xs {
+					batchWeights[i] = batchWeights[i] + err*example[idx]
+				}
 			}
-		}
 
-		for i := range Xs {
-			weights[i] = weights[i] - options.LearningRate*float64(batchWeights[i]/float64(options.BatchSize))
+			for i := range Xs {
+				weightMean := float64(batchWeights[i] / float64(options.BatchSize))
+				penality := options.Alpha * weights[i]
+				weights[i] = weights[i] - options.LearningRate*(weightMean+penality)
+			}
+
+			currentBatch += 1
 		}
 
 		currentEpoch += 1
@@ -81,9 +95,18 @@ func Train(options LinearRegressionOpt) LinearRegressionModel {
 	}
 }
 
-func Fit(model LinearRegressionModel, example []float64, target int) float64 {
+func Fit(model LinearRegressionModel, example []float64) float64 {
 	example = append([]float64{1.0}, example...)
-	return prediction(example, model.Weights, model.Xs) - example[target+1]
+	return prediction(example, model.Weights, model.Xs)
+}
+
+func Mae(preds []float64, real []float64) float64 {
+	sum := 0.0
+	for i := range preds {
+		sum += math.Abs(preds[i] - real[i])
+	}
+
+	return sum / float64(len(preds))
 }
 
 func prediction(example, weights []float64, inputFeatures []int) float64 {
@@ -127,15 +150,12 @@ func initWeights(nWeights int) []float64 {
 	return weights
 }
 
-func createBatch(dataset CSVData, seed Seed, batchSize int) [][]float64 {
-	source := rand.NewPCG(seed.FirstSeed, seed.SeconSeed)
-	gen := rand.New(source)
-
+func createBatch(dataset CSVData, batchSize int) [][]float64 {
 	acc := make([]int, 0, batchSize)
 	batch := make([][]float64, 0, batchSize)
 
 	for len(acc) != batchSize {
-		idx := gen.IntN(len(dataset))
+		idx := rand.IntN(len(dataset))
 		if slices.Contains(acc, idx) {
 			continue
 		}
@@ -181,37 +201,43 @@ func ReadCSV() CSVData {
 	return parsedRecord
 }
 
-func SplitDataset(dataset CSVData, dist Distribution, seed Seed) (trainSet, valSet, testSet CSVData) {
+func SplitDataset(dataset CSVData, dist Distribution, seed Seed) (trainSet, testSet CSVData) {
 	source := rand.NewPCG(seed.FirstSeed, seed.SeconSeed)
 	randGen := rand.New(source)
 
 	total := len(dataset)
 	acc := make([]int, 0, total)
 	trainSize := int(math.Floor(float64(total) * dist.Train))
-	valSize := int(math.Floor(float64(total) * dist.Validation))
 	testSize := int(math.Floor(float64(total) * dist.Test))
 
 	trainSetIdxs, acc := generate(trainSize, total, randGen, acc)
-	valSetIdx, acc := generate(valSize, total, randGen, acc)
 	testSetIdx, acc := generate(testSize, total, randGen, acc)
 
 	if len(acc) < total {
-		sets := [][]int{trainSetIdxs, valSetIdx, testSetIdx}
+		sets := [][]int{trainSetIdxs, testSetIdx}
 
 		// Distibute missing values
 		// using a round-robing strategy
 		sets = distributeMissingValues(sets, acc)
 
 		trainSetIdxs = sets[0]
-		valSetIdx = sets[1]
-		testSetIdx = sets[2]
+		testSetIdx = sets[1]
 	}
 
 	trainSet = createSet(dataset, trainSetIdxs)
-	valSet = createSet(dataset, valSetIdx)
 	testSet = createSet(dataset, testSetIdx)
 
-	return trainSet, valSet, testSet
+	return trainSet, testSet
+}
+
+func GetTarget(train CSVData, target int) []float64 {
+	yTrain := make([]float64, len(train))
+
+	for i, row := range train {
+		yTrain[i] = row[target]
+	}
+
+	return yTrain
 }
 
 func generate(size int, total int, randGen *rand.Rand, acc []int) (elements, accR []int) {
@@ -265,4 +291,60 @@ func distributeMissingValues(sets [][]int, acc []int) [][]int {
 	}
 
 	return sets
+}
+
+func FitStandardScaler(train CSVData, target int) StandardScaler {
+	n := len(train[0])
+	m := len(train)
+	mean := make([]float64, n)
+	std := make([]float64, n)
+
+	// Compute mean
+	for _, row := range train {
+		for j, v := range row {
+			mean[j] += v
+		}
+	}
+
+	for j := range mean {
+		mean[j] /= float64(m)
+	}
+
+	// Compute std deviation
+	for _, row := range train {
+		for j, v := range row {
+			diff := v - mean[j]
+			std[j] += diff * diff
+		}
+	}
+
+	for j := range std {
+		std[j] = math.Sqrt(std[j] / float64(m))
+	}
+
+	return StandardScaler{
+		Mean: mean,
+		Std:  std,
+	}
+}
+
+func Transform(s StandardScaler, train CSVData, target int) CSVData {
+	scaled := make([][]float64, len(train))
+
+	for i, row := range train {
+		scaled[i] = make([]float64, len(row))
+		for j, v := range row {
+			if s.Std[j] == 0 {
+				scaled[i][j] = 0
+			} else {
+				scaled[i][j] = (v - s.Mean[j]) / s.Std[j]
+			}
+		}
+	}
+
+	return scaled
+}
+
+func InverseTransform(s StandardScaler, y float64, targert int) float64 {
+	return y*s.Std[targert] + s.Mean[targert]
 }
